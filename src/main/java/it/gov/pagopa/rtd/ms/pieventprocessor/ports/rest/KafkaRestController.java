ab632d653d78f2a81fef0a779a22cf327c130626
andrea.petreti <andrea.petreti@pagopa.it>
2022-11-04 10:46:29 +0100

    added rest api to simulate tkm event

---
+++ src/main/java/it/gov/pagopa/rtd/ms/pieventprocessor/ports/rest/KafkaRestController.java
@@ -0,0 +1,24 @@
+package it.gov.pagopa.rtd.ms.pieventprocessor.ports.rest;
+
+import it.gov.pagopa.rtd.ms.pieventprocessor.tkm.events.TokenManagerCardChanged;
+import it.gov.pagopa.rtd.ms.pieventprocessor.tkm.events.TokenManagerWalletChanged;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@RequestMapping("")
+@Validated
+public interface KafkaRestController {
+
+  @PutMapping(value = "/tkm-bulk-update")
+  @ResponseStatus(HttpStatus.OK)
+  void sendTkmUpdateEvent(@RequestBody TokenManagerWalletChanged event);
+
+  @PutMapping(value = "/tkm-update")
+  @ResponseStatus(HttpStatus.OK)
+  void sendTkmCardChangedEvent(@RequestBody TokenManagerCardChanged event);
+
+}
